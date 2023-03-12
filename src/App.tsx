ef530c9685ea55e5d5ff95e7abf2ee7ac020aa3d
atejedag <abetejeda1990@gmail.com>
2023-03-11 21:51:54 -0600

    Added edit layer stack window GUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import WelcomePage from './components/WelcomePage';
+import EditLayerStack from './components/EditLayerStack';
 
 const App: React.FC = () => {
   return (
     <div>
       <WelcomePage />
+      {/* <EditLayerStack /> */}
     </div>
   );
 };
