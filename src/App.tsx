77e3a5a021d2000e2fb2f52e1d87691d367dbc3f
Alec <RailgunZx@users.noreply.github.com>
2023-03-12 15:31:44 -0500

    Created Visuals for Initial Structure and Layout Window
    This is the window layout without any of the functionality implemented yet. This component is currently designed to be displayed inside of a window or popup on another page, but it can be modified to fill the screen instead.
    
    The reliability constraints button currently has a mockup drop down button functionality, which only serves as a test of what the real button will do.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 
 import WelcomePage from './components/WelcomePage';
+import InitialStructureAndLayout from './components/InitialStructureAndLayout';
 
 const App: React.FC = () => {
   return (
     <div>
+      {
+      //<InitialStructureAndLayout/>
+      }
       <WelcomePage />
     </div>
   );
