c52f827343a08767226ab5caddaa01ad1c064dab
atejedag <98175251+atejedag@users.noreply.github.com>
2023-03-31 09:01:10 -0500

    Implemented starting GUI for Edit Constraints page (#25)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import WelcomePage from './components/WelcomePage';
+import EditConstraints from './components/EditConstraints';
 
 const App: React.FC = () => {
   return (
     <div>
       <WelcomePage />
+      {/* <EditConstraints /> */}
     </div>
   );
 };
