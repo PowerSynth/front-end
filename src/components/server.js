51f9dddc6dac8b5d4f86101886fe3546c1e2eaa0
Restyled.io <commits@restyled.io>
2023-04-04 22:33:21 +0000

    Restyled by clang-format

--- src/components/server.js
+++ src/components/server.js
@@ -5,19 +5,15 @@ const cors = require('cors');
 app.use(cors());
 
 app.post("/upload", (req, res) => {
-    setTimeout(() => {
-        console.log('file uploaded')
-        return res.status(200).json({ result: true, msg: 'file uploaded'}, 3000);
-    });
-
-
+  setTimeout(() => {
+    console.log('file uploaded')
+    return res.status(200).json({result : true, msg : 'file uploaded'}, 3000);
+  });
 });
 
 app.delete("/upload", (req, res) => {
-    console.log('File deleted')
-    return res.status(200).json({ result: true, msg: 'file deleted'});
+  console.log('File deleted')
+  return res.status(200).json({result : true, msg : 'file deleted'});
 });
 
-app.listen(8080, () => {
-    console.log('Server running on port 8080')
-});
+app.listen(8080, () => {console.log('Server running on port 8080')});
