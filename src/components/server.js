8e823da84433f48ce19a3f220faef16bcc05e5c4
Restyled.io <commits@restyled.io>
2023-04-04 22:33:23 +0000

    Restyled by prettier

--- src/components/server.js
+++ src/components/server.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
 app.post("/upload", (req, res) => {
   setTimeout(() => {
-    console.log('file uploaded')
-    return res.status(200).json({result : true, msg : 'file uploaded'}, 3000);
+    console.log("file uploaded");
+    return res.status(200).json({ result: true, msg: "file uploaded" }, 3000);
   });
 });
 
 app.delete("/upload", (req, res) => {
-  console.log('File deleted')
-  return res.status(200).json({result : true, msg : 'file deleted'});
+  console.log("File deleted");
+  return res.status(200).json({ result: true, msg: "file deleted" });
 });
 
-app.listen(8080, () => {console.log('Server running on port 8080')});
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
