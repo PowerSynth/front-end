c1ea551e60cd4c23f4862e4fc8c7daecb1e673da
Restyled.io <commits@restyled.io>
2023-04-13 15:52:59 +0000

    Restyled by prettier

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,27 @@
 // running for file handling to work To Run: node server.js
 //------------------------------------------------------------------------------------------------------------
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
 // Uploading file
 app.post("/upload", (req, res) => {
   setTimeout(() => {
-    console.log('file uploaded')
-    return res.status(200).json({result : true, msg : 'file uploaded'});
+    console.log("file uploaded");
+    return res.status(200).json({ result: true, msg: "file uploaded" });
   });
 });
 
 // Deleting file
 app.delete("/upload", (req, res) => {
-  console.log('File deleted')
-  return res.status(200).json({result : true, msg : 'file deleted'});
+  console.log("File deleted");
+  return res.status(200).json({ result: true, msg: "file deleted" });
 });
 
 // Running file on port 8080
-app.listen(8080, () => {console.log('Server running on port 8080')});
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
