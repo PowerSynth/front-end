c6adfd62e0f468d3499dda752f642a663cd7a9de
atejedag <abetejeda1990@gmail.com>
2023-04-14 20:11:12 -0500

    Added editable tables for the default datasets

--- src/components/EditConstraints.tsx
+++ src/components/EditConstraints.tsx
@@ -1,28 +1,154 @@
 import React from "react";
+import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css'; // Default Tab styling from react-tabs package, will change later
 import "./EditConstraints.css";
 
-const EditConstraints: React.FC = () => {
-  // arrays for all future headers for other tabs from constraints.csv, otehrs will be implemented later
-  const minDimHeader = ["EMPTY", "power_trace", "signal_trace", "bonding wire pad", "power_lead", "signal_lead", "MOS", "cap"];
-  /*
-  const HorEncHeader = [];
-  const VerEncHeader = [];
-  const HorSpaHeader = [];
-  const VerSpaHeader = [];
-  */
+  type Data = {
+    ROW: any;
+    EMPTY: number;
+    power_trace: number;
+    signal_trace: number;
+    bonding_wire_pad: number;
+    power_lead: number;
+    signal_lead: number;
+    MOS: number;
+    cap: number;
+  }
+
+  type TableProps = {
+    data: Data[];
+    setData: (newData: Data[]) => void;
+  }
+
+// arrays for all future headers for other tabs from constraints.csv, otehrs will be implemented later
+  const tableHeader = ["EMPTY", "power_trace", "signal_trace", "bonding wire pad", "power_lead", "signal_lead", "MOS", "cap"];
 
   // default values include vertical headers due to what I used to input data into cells
-  const MinDimDefaultValues = [
+  const DimData = [
     {ROW: "MinWidth", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 0, power_lead: 3.0, signal_lead: 1.0, MOS: 4.0, cap: 6.0 }, 
     {ROW: "MinLength", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 0, power_lead: 3.0, signal_lead: 1.0, MOS: 6.0, cap: 2.0 },
     {ROW: "MinHorExtension", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 0, power_lead: 3.0, signal_lead: 1.0, MOS: 4.0, cap: 6.0 },
     {ROW: "MinVerExtension", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 0, power_lead: 3.0, signal_lead: 1.0, MOS: 6.0, cap: 2.0 }
   ];
 
+  const DefaultValues = [
+    {ROW: "1", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "2", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "3", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "4", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "5", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "6", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "7", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+    {ROW: "8", EMPTY: 1, power_trace: 1, signal_trace: 1, bonding_wire_pad: 1, power_lead: 1, signal_lead: 1, MOS: 1, cap: 1 },
+  ];
+
+  const horEnc = DefaultValues;
+  const verEnc = DefaultValues;
+  const horSpa = DefaultValues;
+  const verSpa = DefaultValues;
+
+  const Table: React.FC<TableProps> = ({ data, setData }) => {
+    const handleEdit = (index: number, property: keyof Data, value: any) => {
+      const newData = [...data];
+      newData[index] = {...newData[index], [property]: value};
+      setData(newData);
+    };
+
+    return (
+      <table>
+      <thead>
+        <tr>
+          <td className="cellCorner"></td> {/* Modify corner to look cleaner */}
+                {tableHeader.map((rows) => {
+                  return <th scope="col">{rows}</th>
+                })}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((d: any, index) => (
+                  <tr key={index}>
+                    <th> {d.ROW} </th>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.EMPTY}
+                        onChange={(e) => handleEdit(index, "EMPTY", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.power_trace}
+                        onChange={(e) => handleEdit(index, "power_trace", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.signal_trace}
+                        onChange={(e) => handleEdit(index, "signal_trace", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.bonding_wire_pad}
+                        onChange={(e) => handleEdit(index, "bonding_wire_pad", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.power_lead}
+                        onChange={(e) => handleEdit(index, "power_lead", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.signal_lead}
+                        onChange={(e) => handleEdit(index, "signal_lead", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.MOS}
+                        onChange={(e) => handleEdit(index, "MOS", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={d.cap}
+                        onChange={(e) => handleEdit(index, "cap", e.target.value)}
+                      />
+                    </td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    );
+  };
+
+
+
+
+const EditConstraints: React.FC = () => {
+  const [minDimData, setminDimData] = useState(DimData);
+  const [horEncData, sethorEncData] = useState(horEnc);
+  const [verEncData, setverEncData] = useState(verEnc);
+  const [horSpaData, sethorSpaData] = useState(horSpa);
+  const [verSpaData, setverSpaData] = useState(verSpa);
+
   const handleConstraintsContinue = () => { 
     console.log("Edit Constraints Continue")
+    console.log(minDimData);
+    console.log(horEncData);
+    console.log(verEncData);
+    console.log(horSpaData);
+    console.log(verSpaData);
   }; 
 
   return (
@@ -42,47 +168,35 @@ const EditConstraints: React.FC = () => {
           <Tab>MinVerSpacing</Tab>
         </TabList>
 
-        {/* TabPanel is where contents of each tab need to be placed */}
-        <TabPanel> 
-          <div className="min-dimensions">
-            <table>
-              <tr>
-                <td className="cellCorner"></td> {/* Modify corner to look cleaner */}
-                {minDimHeader.map((rows) => {
-                  return <th scope="col">{rows}</th>
-                })}
-              </tr>
-              
-              {/* Input all data rows */}
-              {MinDimDefaultValues.map((val, key)=> {
-                return (
-                  <tr key={key}>
-                    <th>{val.ROW} </th>
-                    <td>{val.EMPTY}</td>
-                    <td>{val.power_trace}</td>
-                    <td>{val.signal_trace}</td>
-                    <td>{val.bonding_wire_pad}</td>
-                    <td>{val.power_lead}</td>
-                    <td>{val.signal_lead}</td>
-                    <td>{val.MOS}</td>
-                    <td>{val.cap}</td>
-                  </tr>
-                )})}
-            </table>
+        <TabPanel>  {/* WILL NEED TO CHANGE/REORGANIZE CLASS NAMES */}
+          <div className="min-dimensions"> 
+            <Table data={minDimData} setData={setminDimData} />
           </div>
         {/* Rest of panels can be implemented later with their respective data */}
         </TabPanel>
+
         <TabPanel>
-          <h2> MinHorEnclosure </h2>
+          <div className="min-dimensions"> 
+            <Table data={horEncData} setData={sethorEncData} />
+          </div>
         </TabPanel>
+
         <TabPanel>
-          <h2> MinVerEnclosure </h2>
+          <div className="min-dimensions">
+            <Table data={verEncData} setData={setverEncData} />
+          </div>
         </TabPanel>
+
         <TabPanel>
-          <h2> MinHorSpacing </h2>
+          <div className="min-dimensions"> 
+            <Table data={horSpaData} setData={sethorSpaData} />
+          </div>
         </TabPanel>
+
         <TabPanel>
-          <h2> MinVerSpacing </h2>
+          <div className="min-dimensions"> 
+            <Table data={verSpaData} setData={setverSpaData} />
+          </div>
         </TabPanel>
 
       </Tabs>
