7c68eb43ecefa27c36566d756439852bc8142dd6
Alec <RailgunZx@users.noreply.github.com>
2023-04-21 19:19:47 -0500

    Create ThermalSetupWIndow.tsx

---
+++ src/components/optimizationSetup/ThermalSetupWIndow.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import "./EditMaterialsWindow.css";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import {
+    Button,
+    FocusStyleManager,
+    Intent
+  } from "@blueprintjs/core";
+
+FocusStyleManager.onlyShowFocusOnTabs();
+
+const ThermalSetupWindow: React.FC = () => {
+
+    return (
+        <div></div>
+    );
+};
+
+export default ThermalSetupWindow;
