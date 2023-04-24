a8fc5b71865ae182ee936839b408847986f53092
Alec <RailgunZx@users.noreply.github.com>
2023-04-23 22:21:38 -0500

    work in progress of the window

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import WelcomePage from './components/WelcomePage';
 import EditConstraints from './components/EditConstraints';
 import EditLayerStack from './components/EditLayerStack';
 import InitialStructureAndLayout from './components/InitialStructureAndLayout';
+import ThermalSetupWindow from './components/optimizationSetup/ThermalSetupWIndow';
 import { Route, Routes } from 'react-router-dom'
 
-const App: React.FC = () => {
+const App: React.FC = () => { //Replaced WelcomePage with ThermalSetupWindow for testing purposes. Leaving this here in case I forget to change it back...
   return (
     <div>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<ThermalSetupWindow />} />
         <Route path="/initial-structure-and-layout" element={<InitialStructureAndLayout />} />
         <Route path="/edit-constraints" element={<EditConstraints />} />
         <Route path="/edit-layer-stack" element={<EditLayerStack />} />
--- src/components/optimizationSetup/ThermalSetupWIndow.tsx
+++ src/components/optimizationSetup/ThermalSetupWIndow.tsx
@@ -1,21 +1,54 @@
 import React from "react";
 import { useState } from "react";
-import "./EditMaterialsWindow.css";
 import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "./ThermalSetupWindow.css";
 import {
     Button,
     FocusStyleManager,
     Intent
   } from "@blueprintjs/core";
+import { Select2 } from "@blueprintjs/select";
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
 const ThermalSetupWindow: React.FC = () => {
 
+
     return (
-        <div></div>
+        <div className="mainDiv">
+            <div className="thermalSetupText">Thermal Setup</div>
+            <div className="modelSelectDiv">
+                <div>Model Select:</div>
+                <input type="text" />
+            </div>
+            <div className="textInputBarDiv">
+                <div>Measure Name:</div>
+                <input className="bp4-input bp4-fill bp4-small" type="text" dir="auto" />
+            </div>
+
+            <div className="midSectionDiv">
+                <div className="tableDiv"></div>
+                <div className="midButtonsDiv">
+                    <div>
+                    <Button small intent={Intent.PRIMARY} className="midButtons">Add Device</Button>
+                    </div>
+                    <div>
+                    <Button small intent={Intent.PRIMARY} className="midButtons">Remove Device</Button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="textInputBarDiv">
+                <div> Heat Convection:</div>
+                <input className="bp4-input bp4-fill bp4-small" type="text" dir="auto" />
+            </div>
+            <div className="textInputBarDiv">
+                <div> Ambient Temperature:</div>
+                <input className="bp4-input bp4-small" type="text" dir="auto" />
+            </div>
+        </div>
     );
 };
 
