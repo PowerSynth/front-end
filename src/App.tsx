68cff5e6977fa932f488216c1025574c650033d0
Alec <RailgunZx@users.noreply.github.com>
2023-04-24 08:11:36 -0500

    Implemented layout of window
    Like the thermal setup window, the table in the middle does not currently have functionality. The "Add Device" and "Remove Device" buttons should add and remove device elements from the table instead of having them manually placed in.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import WelcomePage from './components/WelcomePage';
 import EditConstraints from './components/EditConstraints';
 import EditLayerStack from './components/EditLayerStack';
 import InitialStructureAndLayout from './components/InitialStructureAndLayout';
+import ElectricalSetupWindow from './components/optimizationSetup/ElectricalSetupWindow';
 import { Route, Routes } from 'react-router-dom'
 
 const App: React.FC = () => {
