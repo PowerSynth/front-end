3a418cf2db3aa246f654d71a85c70a61b1c52fce
Restyled.io <commits@restyled.io>
2023-04-25 18:11:11 +0000

    Restyled by clang-format

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------------------------------------
-// Simple backend file to handle uploading/deleting initial files and retreiving results
-// running for file handling to work To Run: node server.js
+// Simple backend file to handle uploading/deleting initial files and retreiving
+// results running for file handling to work To Run: node server.js
 //------------------------------------------------------------------------------------------------------------
 
 const express = require("express");
@@ -13,41 +13,41 @@ app.use(cors());
 app.use(fileUpload());
 app.use(bodyParser.json());
 
-//Temporary json object to store files uploaded by user
+// Temporary json object to store files uploaded by user
 var uploadFiles = {};
-
 
 mainPath = '../../tempFolder/';
 
-//Running PowerSynth request; creating files in folder to run w/ CLI
-//Call CLI from here?
+// Running PowerSynth request; creating files in folder to run w/ CLI
+// Call CLI from here?
 app.post("/api/powersynth", (req, res) => {
   uploadFiles[req.files["file"]["name"]] = req.files["file"]
   console.log(`${req.files["file"]["name"]} uploaded`);
   console.log(req.files)
   // console.log(files)
-  //Handle no files uploaded
+  // Handle no files uploaded
   if (uploadFiles == {}) {
     console.log("you didn't upload the macro script!");
-    return res.status(200).json({ result: true, msg: "You didn't upload macro script!" });
+    return res.status(200).json(
+        {result : true, msg : "You didn't upload macro script!"});
   }
-  //Create file(s) in folder, currently commented out so it doesn't create files every time during testing
+  // Create file(s) in folder, currently commented out so it doesn't create
+  // files every time during testing
   for (var key in uploadFiles) {
-    //uploadFiles[key].mv(mainPath+`${uploadFiles[key]["name"]}`)
+    // uploadFiles[key].mv(mainPath+`${uploadFiles[key]["name"]}`)
   }
-  //Remove file(s) from temporary json object
+  // Remove file(s) from temporary json object
   for (var key in uploadFiles) {
     delete uploadFiles[key];
   }
-  return res.status(200).json({ result: true, msg: "Running PowerSynth", results: req.files});
- 
+  return res.status(200).json(
+      {result : true, msg : "Running PowerSynth", results : req.files});
 })
 
 app.get("/download", (req, res) => {
-  res.download("../src/components/solutionBrowser/testImages/initial_layout_l1.png");
+  res.download(
+      "../src/components/solutionBrowser/testImages/initial_layout_l1.png");
 })
 
 // Running file on port 8080
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
-});
+app.listen(8080, () => { console.log("Server running on port 8080"); });
