fd4530dd4eb0678e1333491bae36c42914447401
Restyled.io <commits@restyled.io>
2023-04-25 18:11:14 +0000

    Restyled by prettier

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,21 @@ app.use(bodyParser.json());
 // Temporary json object to store files uploaded by user
 var uploadFiles = {};
 
-mainPath = '../../tempFolder/';
+mainPath = "../../tempFolder/";
 
 // Running PowerSynth request; creating files in folder to run w/ CLI
 // Call CLI from here?
 app.post("/api/powersynth", (req, res) => {
-  uploadFiles[req.files["file"]["name"]] = req.files["file"]
+  uploadFiles[req.files["file"]["name"]] = req.files["file"];
   console.log(`${req.files["file"]["name"]} uploaded`);
-  console.log(req.files)
+  console.log(req.files);
   // console.log(files)
   // Handle no files uploaded
   if (uploadFiles == {}) {
     console.log("you didn't upload the macro script!");
-    return res.status(200).json(
-        {result : true, msg : "You didn't upload macro script!"});
+    return res
+      .status(200)
+      .json({ result: true, msg: "You didn't upload macro script!" });
   }
   // Create file(s) in folder, currently commented out so it doesn't create
   // files every time during testing
@@ -40,14 +41,18 @@ app.post("/api/powersynth", (req, res) => {
   for (var key in uploadFiles) {
     delete uploadFiles[key];
   }
-  return res.status(200).json(
-      {result : true, msg : "Running PowerSynth", results : req.files});
-})
+  return res
+    .status(200)
+    .json({ result: true, msg: "Running PowerSynth", results: req.files });
+});
 
 app.get("/download", (req, res) => {
   res.download(
-      "../src/components/solutionBrowser/testImages/initial_layout_l1.png");
-})
+    "../src/components/solutionBrowser/testImages/initial_layout_l1.png"
+  );
+});
 
 // Running file on port 8080
-app.listen(8080, () => { console.log("Server running on port 8080"); });
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
